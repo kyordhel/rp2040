67bc9831fd37639012b4041584e27354e5e44440
kyordhel <kyordhel@gmail.com>
2023-09-18 00:26:19 -0600

    Adds tempC project that reads the RP2040 core temperature.

---
+++ tempC/main.c
@@ -0,0 +1,77 @@
+/* Kernel includes. */
+#include <FreeRTOS.h>
+#include <task.h>
+
+/* Library includes. */
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include <hardware/gpio.h>
+#include <hardware/adc.h>
+
+/**
+ * Conversion constant
+ */
+#define K -0.029259019f
+
+/**
+ * Configures the hardware
+ */
+static void setup();
+
+/**
+ * The task that will blink the led.
+ * @param  Parameter passed to the task. Not used.
+ */
+static void blinkTask( void *param );
+
+
+void main( void ){
+	setup();
+	printf("Starting blink.\n");
+
+	xTaskCreate( blinkTask,                 /* The function that implements the task. */
+				 "blink",                   /* The text name assigned to the task - for debug only as it is not used by the kernel. */
+				 configMINIMAL_STACK_SIZE,  /* The size of the stack to allocate to the task. */
+				 NULL,                      /* The parameter passed to the task - not used in this case. */
+				 1,                         /* The priority assigned to the task. */
+				 NULL );                    /* The task handle is not required, so NULL is passed. */
+
+	vTaskStartScheduler();
+	/* If all is well, the scheduler will now be running, and the following
+	line will never be reached.  If the following line does execute, then
+	there was insufficient FreeRTOS heap memory available for the Idle and/or
+	timer tasks to be created.  See the memory management section on the
+	FreeRTOS web site for more details on the FreeRTOS heap
+	http://www.freertos.org/a00111.html. */
+	for( ;; );
+}
+
+static void setup(){
+	stdio_init_all();
+	adc_init();
+	adc_set_temp_sensor_enabled(true);
+	adc_select_input(4); // Internal temperature sensor
+
+	gpio_init(PICO_DEFAULT_LED_PIN);
+	gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+	gpio_put(PICO_DEFAULT_LED_PIN, 0);
+}
+
+
+static void blinkTask( void *param ){
+	int value = 0;
+	uint16_t raw;
+	float vADC;
+	float temp;
+
+	while(true){
+		raw = adc_read();
+		vADC = raw * 3.3f / 4095;
+		temp = 27 - (vADC - 0.706)/0.001721;
+		printf("Temperature: %0.0fC | VADC: %0.3fV | Raw: %u\n", temp, vADC, raw);
+		gpio_put(PICO_DEFAULT_LED_PIN, value);
+		value = !value;
+		vTaskDelay(pdMS_TO_TICKS(1000));
+	}
+
+}
